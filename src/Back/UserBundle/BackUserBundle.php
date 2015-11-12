13baf37ac68ab044848a2b8ae6af2b3ae5a4d9c5
zied666 <zied.kharraz@gmail.com>
2015-11-12 23:50:18 +0100

    registratuin overide

--- src/Back/UserBundle/BackUserBundle.php
+++ src/Back/UserBundle/BackUserBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BackUserBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
---
+++ src/Back/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Back\UserBundle\Controller;
+
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends BaseController
+{
+    public function registerAction(Request $request)
+    {
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        $userManager = $this->get('fos_user.user_manager');
+        $dispatcher = $this->get('event_dispatcher');
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse())
+            return $event->getResponse();
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+            $userManager->updateUser($user);
+
+            if (null === $response = $event->getResponse())
+            {
+                if ($user->hasRole('ROLE_BABYSITTER'))
+                    $response = new RedirectResponse($this->generateUrl('Front_BabySitter_Profile_myprofile'));
+                else
+                    $response = new RedirectResponse($this->generateUrl('fos_user_registration_confirmed'));
+            }
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+            return $response;
+        }
+
+        return $this->render('FOSUserBundle:Registration:register.html.twig', array('form' => $form->createView(),));
+    }
+}
