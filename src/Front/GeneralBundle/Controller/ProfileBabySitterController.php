f38160a612d195b57c57118ed78f07d4a40ba2f3
Zied Kharraz <zied.kharraz@gmail.com>
2016-02-14 12:41:34 +0100

    add google calendar

--- src/Front/GeneralBundle/Controller/ProfileBabySitterController.php
+++ src/Front/GeneralBundle/Controller/ProfileBabySitterController.php
@@ -15,31 +15,28 @@ class ProfileBabySitterController extends Controller
 {
     public function profileAction(Request $request)
     {
-        $em=$this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
-        if(!is_null($user->getBabySitter()))
-            $babysitter=$user->getBabySitter();
-        else
-        {
-            $babysitter=new BabySitter();
+        if (!is_null($user->getBabySitter()))
+            $babysitter = $user->getBabySitter();
+        else {
+            $babysitter = new BabySitter();
             $babysitter->setLastName($user->getLastName())->setFirstName($user->getFirstName())->setEmail($user->getEmail());
         }
-        $form=$this->createForm(new BabySitterType(),$babysitter);
-        if($request->isMethod('POST'))
-        {
+        $form = $this->createForm(new BabySitterType(), $babysitter);
+        if ($request->isMethod('POST')) {
             $form->submit($request);
-            if($form->isValid())
-            {
-                $babysitter=$form->getData();
+            if ($form->isValid()) {
+                $babysitter = $form->getData();
                 $em->persist($babysitter);
                 $em->persist($user->setBabySitter($babysitter));
                 $em->flush();
                 return $this->redirect($this->generateUrl('Front_BabySitter_Profile_myprofile'));
             }
         }
-        return $this->render('FrontGeneralBundle:ProfileBabySitter:MyProfile.html.twig',array(
-            'form'=>$form->createView(),
-            'babysitter'=>$babysitter
+        return $this->render('FrontGeneralBundle:ProfileBabySitter:MyProfile.html.twig', array(
+            'form'       => $form->createView(),
+            'babysitter' => $babysitter
         ));
     }
 
@@ -56,79 +53,73 @@ class ProfileBabySitterController extends Controller
         return $response;
     }
 
-    public function calendarAction($id,Request $request)
+    public function calendarAction($id, Request $request)
     {
-        $em=$this->get('doctrine.orm.entity_manager');
-        $user=$this->getUser();
-        if(!is_null($id))
-            $calendrier=$em->find('BackUserBundle:Calendrier',$id);
+        $em = $this->get('doctrine.orm.entity_manager');
+        $user = $this->getUser();
+        if (!is_null($id))
+            $calendrier = $em->find('BackUserBundle:Calendrier', $id);
         else
-            $calendrier=new Calendrier();
-        $calendriers=$em->getRepository('BackUserBundle:Calendrier')->findBy(array('babysitter'=>$user->getBabysitter()),array('year'=>"desc",'month'=>"desc"));
-        $form=$this->createForm(new CalendrierType(),$calendrier);
-        if($request->isMethod('POST'))
-        {
+            $calendrier = new Calendrier();
+        $calendriers = $em->getRepository('BackUserBundle:Calendrier')->findBy(array('babysitter' => $user->getBabysitter()), array('year' => "desc", 'month' => "desc"));
+        $form = $this->createForm(new CalendrierType(), $calendrier);
+        if ($request->isMethod('POST')) {
             $form->handleRequest($request);
-            if($form->isValid())
-            {
-                $calendrier=$form->getData();
-                $verif=$em->getRepository('BackUserBundle:Calendrier')->findOneBy(array(
-                    'month'=>$calendrier->getMonth(),
-                    'year'=>$calendrier->getYear(),
+            if ($form->isValid()) {
+                $calendrier = $form->getData();
+                $verif = $em->getRepository('BackUserBundle:Calendrier')->findOneBy(array(
+                    'month' => $calendrier->getMonth(),
+                    'year'  => $calendrier->getYear(),
                 ));
-                if($verif and is_null($id))
-                {
-                    $this->addFlash("danger","You have already entered this date");
+                if ($verif and is_null($id)) {
+                    $this->addFlash("danger", "You have already entered this date");
                     return $this->redirectToRoute("Front_BabySitter_Profile_mycalendar");
                 }
                 $em->persist($calendrier->setBabysitter($user->getBabysitter()));
                 $em->flush();
-                $this->addFlash("success","success");
+                $this->addFlash("success", "success");
                 return $this->redirectToRoute("Front_BabySitter_Profile_mycalendar");
             }
         }
-        return $this->render('FrontGeneralBundle:ProfileBabySitter:MyCalendar.html.twig',array(
-            'form'=>$form->createView(),
-            'calendars'=>$calendriers
+        return $this->render('FrontGeneralBundle:ProfileBabySitter:MyCalendar.html.twig', array(
+            'form'      => $form->createView(),
+            'calendars' => $calendriers
         ));
     }
 
     public function listBookingAction($page)
     {
-        $em=$this->get('doctrine.orm.entity_manager');
-        $user=$this->getUser();
-        $booking=$em->getRepository('BackUserBundle:Booking')->findBy(array('babysitter'=>$user->getBabysitter()),array('id'=>'desc'));
-        $bookings = $this->get('knp_paginator')->paginate($booking,$page,20);
-        return $this->render('FrontGeneralBundle:ProfileBabySitter:listBooking.html.twig',array(
-            'bookings'=>$bookings
+        $em = $this->get('doctrine.orm.entity_manager');
+        $user = $this->getUser();
+        $booking = $em->getRepository('BackUserBundle:Booking')->findBy(array('babysitter' => $user->getBabysitter()), array('id' => 'desc'));
+        $validatedBookings = $em->getRepository('BackUserBundle:Booking')->findBy(array('babysitter' => $user->getBabysitter(),'status'=>3));
+        $bookings = $this->get('knp_paginator')->paginate($booking, $page, 20);
+        return $this->render('FrontGeneralBundle:ProfileBabySitter:listBooking.html.twig', array(
+            'bookings'          => $bookings,
+            'validatedBookings' => $validatedBookings
         ));
     }
 
     public function validateAction(Booking $booking)
     {
-        $em=$this->get('doctrine.orm.entity_manager');
-        if($booking->getUser()->getId()==$this->getUser()->getId())
-        {
-            $booking->setStatus(3);
-            $em->persist($booking);
-            $em->flush();
-            $this->get('mailerservice')->sendBookingValidated($booking);
-            $this->addFlash('success','success');
-        }
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $booking->setStatus(3);
+        $em->persist($booking);
+        $em->flush();
+        $this->get('mailerservice')->sendBookingValidated($booking);
+        $this->addFlash('success', 'success');
         return $this->redirectToRoute('Front_BabySitter_Profile_listBooking');
     }
 
     public function cancelAction(Booking $booking)
     {
-        $em=$this->get('doctrine.orm.entity_manager');
-        if($booking->getUser()->getId()==$this->getUser()->getId())
-        {
-            $booking->setStatus(4);
-            $em->persist($booking);
-            $em->flush();
-            $this->get('mailerservice')->sendBookingCanceled($booking);
-            $this->addFlash('success','success');
-        }
+        $em = $this->get('doctrine.orm.entity_manager');
+        $booking->setStatus(4);
+        $em->persist($booking);
+        $em->flush();
+        $this->get('mailerservice')->sendBookingCanceled($booking);
+        $this->addFlash('success', 'success');
         return $this->redirectToRoute('Front_BabySitter_Profile_listBooking');
     }
 
